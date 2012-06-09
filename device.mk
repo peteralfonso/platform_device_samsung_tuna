0e476e9d72c6e95aa0411c6fd4bc81ae5dbb0b4e
Peter Alfonso <peteralfonsojr@gmail.com>
2012-06-09 16:28:07 -0400

    Add SIM toolkit

--- device.mk
+++ device.mk
@@ -83,6 +83,9 @@ PRODUCT_PROPERTY_OVERRIDES := \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
 
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk
 
 # NFC
 PRODUCT_PACKAGES += \
